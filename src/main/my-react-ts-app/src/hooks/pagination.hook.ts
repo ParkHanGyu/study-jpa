e7276113b6b3f2e5010365c43f4958a463100796
DESKTOP-SFN0IMB\PHG <qkrgksrl0033@naver.com>
2024-03-30 00:14:47 +0900

    페이징 처리 함수 작성중

--- src/main/my-react-ts-app/src/hooks/pagination.hook.ts
+++ src/main/my-react-ts-app/src/hooks/pagination.hook.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
-const usePagination = <T>(countPerPage: number ) => {
+// countPerPage : 한페이지당 몇개 보여줄지
+const usePagination = <T>(countPerPage: number) => {
   //  state: 전체 객체 리스트 상태   //
   const [totalList, setTotalList] = useState<T[]>([]);
 
@@ -25,10 +26,52 @@ const usePagination = <T>(countPerPage: number ) => {
   //  state: 전체 섹션 상태   //
   const [totalSection, setTotalSection] = useState<number>(1);
 
+  //   function: 보여줄 객체 리스트 추출 함수   //
+  const setView = () => {
+    const FIRST_INDEX = countPerPage * (currentPage - 1);
+    const LAST_INDEX =
+      totalList.length > countPerPage * currentPage
+        ? countPerPage * currentPage
+        : totalList.length;
+    const viewList = totalList.slice(FIRST_INDEX, LAST_INDEX);
+    setViewList(viewList);
+  };
+
+  //   function: 보여줄 페이지 리스트 추출 함수   //
+  const setViewPage = () => {
+    const FIRST_INDEX = 10 * (currentSection - 1);
+    const LAST_INDEX =
+      totalPageList.length > 10 * currentSection
+        ? 10 * currentSection
+        : totalPageList.length;
+    const viewPageList = totalPageList.slice(FIRST_INDEX, LAST_INDEX);
+    setViewPageList(viewPageList);
+  };
+
   //   effect: totalList가 변경될 때마다 실행할 작업   //
   useEffect(() => {
-    const totalPage = 
-  },[totalList] )
+    const totalPage = Math.ceil(totalList.length / countPerPage);
+    const totalPageList: number[] = [];
+    for (let page = 1; page <= totalPage; page++) {
+      totalPageList.push(page);
+    }
+    setTotalPageList(totalPageList);
+
+    const totalSection = Math.ceil(totalList.length / (countPerPage * 10));
+    setTotalSection(totalSection);
+
+    setCurrentPage(1);
+    setCurrentSection(1);
+
+    setView();
+    setViewPage();
+  }, [totalList]);
+
+  //   effect: current page가 변경될 때마다 실행할 작업   //
+  useEffect(() => setView, [currentPage]);
+
+  //   effect: current section이 변경될 때마다 실행할 작업   //
+  useEffect(() => setViewPage, [currentPage]);
 
   return {
     // 현재 페이지가 어떤 페이지에 있는지
@@ -47,5 +90,5 @@ const usePagination = <T>(countPerPage: number ) => {
 
 export default usePagination;
 
-// https://www.youtube.com/watch?v=zVwqr7mwQc8
-// 16:31부터
+// https://www.youtube.com/watch?v=S3YvpwyC-F4
+// 처음부터 보기
